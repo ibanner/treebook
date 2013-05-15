5641777eced71ca486f9a0e983cd5cf042f8da3e
Itay Banner <itay@ibanner.co.il>
2013-05-16 00:59:14 +0300

    Added state_machine gem, created user_notifier mailer

--- test/unit/user_friendship_test.rb
+++ test/unit/user_friendship_test.rb
@@ -14,4 +14,26 @@ class UserFriendshipTest < ActiveSupport::TestCase
     UserFriendship.create user_id: users(:itay).id, friend_id: users(:user3).id
     assert users(:itay).friends.include?(users(:user3))
   end
+
+  context "a new instance" do
+    setup do
+      @user_friendship = UserFriendship.new user: users(:itay), friend: users(:user3)
+    end
+
+    should "have a pending state" do
+      assert_equal "pending", @user_friendship.state
+    end
+  end
+
+  context "#send_request_email" do
+    setup do
+      @user_friendship = UserFriendship.create user: users(:itay), friend: users(:user3)
+    end
+
+    should "send an email" do
+      assert_difference "ActionMailer::Base.deliveries.size", 1 do
+        @user_friendship.send_request_email
+      end
+    end
+  end
 end
